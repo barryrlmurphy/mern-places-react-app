445648971bb897902ba39f29a00c460a1c6cac33
barryrlmurphy <barryrlmurphy@hotmail.com>
2020-09-17 12:11:13 +0100

    Added dynamic places

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Mountain of Power",
+    imageUrl:
+      "https://i.pinimg.com/originals/98/05/d8/9805d830026d43aa4f46794f4cc2bee2.jpg",
+    address: "Stygia",
+    location: { lat: 62.8055102, lng: 99.571015 },
+    creator: "u3",
+  },
+  {
+    id: "p2",
+    title: "Tower of the Serpent",
+    imageUrl:
+      "https://static.wikia.nocookie.net/conan/images/7/71/Tower_of_the_Serpent.jpg/revision/latest/scale-to-width-down/275?cb=20131022022919",
+    address: "Stygia",
+    location: { lat: 68.4232544, lng: 109.4159828 },
+    creator: "u3",
+  },
+  {
+    id: "p3",
+    title: "Zamora",
+    imageUrl:
+      "https://i.pinimg.com/originals/ce/a5/19/cea5192a244fc9ff4b371d2bc387e67e.jpg",
+    address: "Zamora",
+    location: { lat: 67.957965, lng: 109.547819 },
+    creator: "u1",
+  },
+  {
+    id: "p4",
+    title: "Cimmeria",
+    imageUrl: "https://hyboria.xoth.net/maps/hwc_cimmeria.jpg",
+    address: "Cimmeria",
+    location: { lat: 72.000434, lng: 86.825358 },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = (props) => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,16 +5,23 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Max Von Sydow",
+      name: "King Osric",
       image:
-        "https://i.pinimg.com/originals/a8/61/e6/a861e6fb0aab90e66e78607360613c53.jpg",
+        "https://static.wikia.nocookie.net/conan/images/1/17/King_Osric.jpg/revision/latest/scale-to-width-down/269?cb=20131022030346",
       places: 3,
     },
     {
       id: "u2",
-      name: "Arnold Swarzenegger",
+      name: "Conan",
       image:
         "https://i.guim.co.uk/img/media/65591ba419de07f2f7acc6abc964d85fe7187c05/0_269_1782_1069/master/1782..jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=aa9a8a04848641d29c896baa79178487",
+      places: 1,
+    },
+    {
+      id: "u3",
+      name: "Thulsa Doom",
+      image:
+        "https://comicvine1.cbsistatic.com/uploads/scale_small/9/98260/2150838-19179107.jpg",
       places: 1,
     },
   ];
